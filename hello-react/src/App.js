666826571e50472121026dd61be76e0e672f5f41
Ian Homer <code@ianhomer.com>
2025-08-07 08:47:47 +0100

    ci: add react hello world app and npm audit to CI

---
+++ hello-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, World!</h1>
+      <p>Welcome to your first React application.</p>
+    </div>
+  );
+}
+
+export default App;
